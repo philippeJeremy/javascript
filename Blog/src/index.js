613315e2ab540e4d08ce59dae6bbc2df64c12c0e
jeremy <jeremy561000@gmail.com>
2023-01-27 09:10:46 +0100

    en cours

---
+++ Blog/src/index.js
@@ -0,0 +1,63 @@
+import "./assets/styles/styles.scss";
+import "./index.scss";
+
+const articleContainerElement = document.querySelector(".articles-container");
+
+const createArticles = articles => {
+    const articlesDOM = articles.map(article => {
+        const articleDOM = document.createElement("div");
+        articleDOM.classList.add("article");
+        articleDOM.innerHTML = `
+<img
+  src="${article.img}"
+  alt="profile"
+/>
+<h2>${article.title}</h2>
+<p class="article-author">${article.author} - ${article.category}</p>
+<p class="article-content">
+  ${article.content}
+</p>
+<div class="article-actions">
+  <button class="btn btn-danger" data-id=${article._id} >Supprimer</button>
+</div>
+`;
+        return articleDOM;
+    });
+    articleContainerElement.innerHTML = "";
+    articleContainerElement.append(...articlesDOM);
+    const deleteButtons = articleContainerElement.querySelectorAll(".btn-danger");
+    deleteButtons.forEach(button => {
+        button.addEventListener("click", async event => {
+            try {
+                const target = event.target;
+                const articleId = target.dataset.id;
+                const response = await fetch(
+                    `https://restapi.fr/api/article/${articleId}`,
+                    {
+                        method: "DELETE"
+                    }
+                );
+                const body = await response.json();
+                console.log(body);
+                fetchArticle();
+            } catch (e) {
+                console.log("e : ", e);
+            }
+        });
+    });
+};
+
+const fetchArticle = async () => {
+    try {
+        const response = await fetch("https://restapi.fr/api/article");
+        let articles = await response.json();
+        if (!Array.isArray(articles)) {
+            articles = [articles];
+        }
+        createArticles(articles);
+    } catch (e) {
+        console.log("e : ", e);
+    }
+};
+
+fetchArticle();
